85fbfacbdeac350ff18e3568ad2032d9e308690a
Tifanny Rivera <trivera51580@gmail.com>
2022-08-29 17:11:04 -0600

    fixed header hrefs

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,22 +1,24 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
-import logo from '../../logo.png'
-import './header.css'
+import logo from "../../logo.png";
+import "./header.css";
 
 function Header() {
   return (
     <Navbar collapseOnSelect expand="lg" className="navbar">
-        <Navbar.Brand href="#home"><img src={logo} alt="logo" /></Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto"></Nav>
-          <Nav >
-            <Nav.Link href="#deets">Credentials</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Contact
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+      <Navbar.Brand href="#home">
+        <img src={logo} alt="logo" />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="me-auto"></Nav>
+        <Nav>
+          <Nav.Link href="#credentials">Credentials</Nav.Link>
+          <Nav.Link eventKey={2} href="#contact">
+            Contact
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 }
