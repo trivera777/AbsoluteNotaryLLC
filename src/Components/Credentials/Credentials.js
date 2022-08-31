10cf62c7fde2b9d7b35d60f7bd907958ad3d569c
Tifanny Rivera <trivera51580@gmail.com>
2022-08-31 15:53:02 -0600

    added card to credentials component, not sure if I want to keep it

--- src/Components/Credentials/Credentials.js
+++ src/Components/Credentials/Credentials.js
@@ -1,13 +1,21 @@
 import React from "react";
 import "./credentials.css";
 import { Card } from "react-bootstrap";
-import certificate from '../../lss.png'
-
+import certificate from "../../lss.png";
 
 function Credentials() {
   return (
     <div className="credentials" id="credentials">
-      <img src={certificate} alt="Loan Signing System Certificate" />
+      <Card>
+        <Card.Header>Credentials</Card.Header>
+        <Card.Body>
+          <blockquote className="blockquote mb-0">
+            <p>
+              <img src={certificate} alt="Loan Signing System Certificate" />
+            </p>
+          </blockquote>
+        </Card.Body>
+      </Card>
     </div>
   );
 }
