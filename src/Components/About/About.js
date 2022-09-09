316edd8ee26eee5dfc5454121ae1fbc6dbf67963
Tifanny Rivera <trivera51580@gmail.com>
2022-09-09 07:54:43 -0600

    working credentials images

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -8,25 +8,22 @@ function About() {
       <Card>
         <Card.Body>
           <blockquote className="blockquote mb-0">
-        <h2>About</h2>
+            <h2>about.</h2>
             <p>
               {" "}
-              I am a professional and certified NNA Notary Public & Notary Loan
-              Signing Agent. I am also certified with the Loan Signing System. I
-              offer 3 years of notary service experience as well as 5 years
-              experience working in a financial institution. This is why I am
-              your go to signing agent - I am very knowledgeable when it comes
-              to loan documents and know how crucial it is to make zero
-              mistakes. That is why I never miss a signature, date or initials
-              and I always make same day drop offs. I will assure the signing
-              process goes smoothly for the client and I can confidently give a
-              brief overviews, if needed, without stepping outside the scope of
-              my roles as a signing agent. I look forward to being your
-              preferred signing agent!{" "}
+              I am a professional and certified NNA Notary Loan Signing Agent. I
+              am also certified with the Loan Signing System. I offer 3 years of
+              notary service experience as well as 5 years experience working in
+              a financial institution. This is why I am your go to signing agent
+              - I am very knowledgeable when it comes to loan documents and know
+              how crucial it is to make zero mistakes. That is why I never miss
+              a signature, date or initials and I always make same day drop
+              offs. I will assure the signing process goes smoothly for the
+              client and I can confidently give a brief overviews, if needed,
+              without stepping outside the scope of my roles as a signing agent.
+              I look forward to being your preferred signing agent!{" "}
             </p>
-            <footer className="blockquote-footer">
-              Tifanny Rivera
-            </footer>
+            <footer className="blockquote-footer">Tifanny Rivera</footer>
           </blockquote>
         </Card.Body>
       </Card>
--- src/Components/Credentials/Credentials.js
+++ src/Components/Credentials/Credentials.js
@@ -1,24 +1,54 @@
 import React from "react";
 import "./credentials.css";
-// import { Card } from "react-bootstrap";
-// import certificate from "../../lss.png";
-// import nsa from "../../nsa.png";
+import { Card } from "react-bootstrap";
+import certificate from "../../lss.png";
+import nsa from "../../nsa.png";
 
 function Credentials() {
   return (
     <div className="credentials" id="credentials">
-        credentials
-      {/* <Card>
-        <Card.Header>Credentials</Card.Header>
+      <Card>
         <Card.Body>
           <blockquote className="blockquote mb-0">
-            <p>
-              <img src={certificate} alt="Loan Signing System Certificate" />
-              <img src={nsa} alt="Loan Signing System Certificate" />
-            </p>
+            <h2>credentials.</h2>
+            <Card>
+              <Card.Img 
+              variant="top" 
+              className="nsaImg" 
+              src={nsa} 
+              style={{
+                height: "200px",
+                width: "200px"
+              }}
+              />
+              <Card.Body>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+            <br />
+            <Card>
+              <Card.Img
+                variant="top"
+                className="certificateImg"
+                src={certificate}
+                style={{
+                  height: "200px",
+                  width: "280px"
+                }}
+              />
+              <Card.Body>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+              </Card.Body>
+            </Card>
           </blockquote>
         </Card.Body>
-      </Card> */}
+      </Card>
     </div>
   );
 }
