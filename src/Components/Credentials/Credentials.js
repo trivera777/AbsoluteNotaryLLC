96e4a8886008c55b12b25fc04807375c03d9d927
Tifanny Rivera <trivera51580@gmail.com>
2022-09-11 21:44:51 -0600

    credentials component done for now. not sure if I will keep it

--- src/Components/Credentials/Credentials.js
+++ src/Components/Credentials/Credentials.js
@@ -1,14 +1,37 @@
 import React from "react";
 import "./credentials.css";
-import { Card } from "react-bootstrap";
+import { Card, Figure } from "react-bootstrap";
 import certificate from "./lss.png";
 import nsa from "./nsa.png";
 import nsabc from "./nsabc.png";
+import badge from "./badge.png";
 
 function Credentials() {
   return (
     <div className="credentials" id="credentials">
-      <img
+      <h2>credentials.</h2>
+      <Figure className="credentialsContainer">
+      <Figure.Image
+        width={171}
+        height={180}
+        alt="171x180"
+        src={nsabc}
+      />
+      <Figure.Image
+        width={171}
+        height={180}
+        alt="171x180"
+        src={nsa}
+      />
+      <Figure.Image
+        width={250}
+        height={180}
+        alt="171x180"
+        src={certificate}
+      />
+    </Figure>
+
+      {/* <img
         src={nsa}
         alt="NNA Certificate"
         style={{
@@ -31,7 +54,7 @@ function Credentials() {
           height: "200px",
           width: "280px",
         }}
-      />
+      /> */}
     </div>
   );
 }
