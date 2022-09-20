a30c79c06b00d24a0741f4384551f484cc96b09d
Tifanny Rivera <trivera51580@gmail.com>
2022-09-19 22:13:22 -0600

    added contact image

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,10 +1,13 @@
 import React from "react";
+import email from "./email.png";
 import './contact.css'
 
 function Contact(){
     return (
         <div className="contact" id="contact">
             contact
+          <img src={email} className="files" alt="Files" />
+
         </div>
     )
 }
