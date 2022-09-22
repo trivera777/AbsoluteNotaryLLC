653778bf930341f4bd46dabcad9c7c2147990f28
Tifanny Rivera <trivera51580@gmail.com>
2022-09-21 20:42:40 -0600

    working contact component

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -2,15 +2,20 @@ import React from "react";
 import email from "./email.png";
 import "./contact.css";
 import { Divider } from "@mui/material";
+import { LocalPhone, Email } from "@mui/icons-material";
 
 function Contact() {
   return (
     <div className="contact" id="contact">
-      <Divider textAlign="middle">CONTACT</Divider>
-        <div className="contactContainer">
-
-      <img src={email} className="files" alt="Files" />
-        </div>
+      <Divider textAlign="left">CONTACT</Divider>
+      <div className="contactContainer">
+        <img src={email} className="files" alt="Files" />
+        <ul>
+          <LocalPhone /> 970-515-8586
+          <br />
+          <Email /> trivera@absolutenotaryco.com
+        </ul>
+      </div>
     </div>
   );
 }
