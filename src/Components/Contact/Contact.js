deb680906b9594944c06dcb72197be0fe9c37086
Tifanny Rivera <trivera51580@gmail.com>
2022-09-21 19:13:25 -0600

    added divider to contact component

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,11 +1,14 @@
 import React from "react";
 import email from "./email.png";
 import './contact.css'
+import { Divider } from "@mui/material";
 
 function Contact(){
     return (
-        <div className="contact" id="contact">
-            contact
+        <div className="contactContainer" id="contact">
+          
+      <Divider textAlign="right">CONTACT</Divider>
+
           <img src={email} className="files" alt="Files" />
 
         </div>
